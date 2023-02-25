fba53db7af48083e7a95272ff823d99149ea5a6a
Shane Standifur <shanestandifur@gmail.com>
2023-02-25 12:32:05 -0600

    back end code that adds a user to the mailchimp contacts and sends welcome email

--- avengersassembleapi/views/auth.py
+++ avengersassembleapi/views/auth.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.http import HttpResponseNotAllowed, JsonResponse
 import requests
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from mailchimp3 import MailChimp
-from mailchimp3.helpers import get_subscriber_hash
 from avengersassembleapi.models import AvengerUser
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.middleware.csrf import get_token
+import json
 
 CORS_ORIGIN_WHITELIST = [    "http://localhost:3000",    "http://127.0.0.1:3000"]
 
@@ -49,16 +49,7 @@ def login_user(request):
 @permission_classes([AllowAny])
 
 
-def add_subscriber_to_mailchimp(email):
-    client = MailChimp(mc_api='5eda1bb3ed659f9cdb147fabc067cc91-us21', mc_user='shanestandifur@gmail.com')
-    list_id = '2da45402b8'
-    email_hash = get_subscriber_hash(email)
-    data = {
-        'email_address': email,
-        'status': 'subscribed'
-    }
-    client.lists.members.create(list_id=list_id, data=data)
-
+@csrf_exempt
 @ensure_csrf_cookie
 def register_user(request):
     '''Handles the creation of a new gamer for authentication
@@ -66,39 +57,51 @@ def register_user(request):
     Method arguments:
       request -- The full HTTP request object
     '''
-
-    csrf_token = get_token(request)
     
-    new_user = User.objects.create_user(
-        username=request.data['username'],
-        password=request.data['password'],
-        first_name=request.data['first_name'],
-        last_name=request.data['last_name'],
-        email= request.data['email']
-    )
+    if request.method == 'POST':
+        # get JSON data from request body
+        data = json.loads(request.body)
+        
+        # create new user with data
+        new_user = User.objects.create_user(
+            username=data['username'],
+            password=data['password'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email']
+        )
 
-    avenger = AvengerUser.objects.create(
-        user=new_user
-    )
+        avenger = AvengerUser.objects.create(
+            user=new_user
+        )
 
-    # Use the REST Framework's token generator on the new user account
-    token = Token.objects.create(user=avenger.user)
-    mailchimp_url = "https://<datacenter>.api.mailchimp.com/3.0/lists/2da45402b8/members"
-    mailchimp_data = {
-        "email_address": new_user.email,
-        "status": "subscribed",
-        "merge_fields": {
-            "FNAME": new_user.first_name,
-            "LNAME": new_user.last_name
+        # Use the REST Framework's token generator on the new user account
+        token = Token.objects.create(user=avenger.user)
+        mailchimp_url = "https://us21.api.mailchimp.com/3.0/lists/AUDIENCE_ID/members"
+        mailchimp_data = {
+            "email_address": new_user.email,
+            "status": "subscribed",
+            "merge_fields": {
+                "FNAME": new_user.first_name,
+                "LNAME": new_user.last_name
+            }
         }
-    }
-    MAILCHIMP_API_KEY = '5eda1bb3ed659f9cdb147fabc067cc91-us21'
-    mailchimp_auth = ("", MAILCHIMP_API_KEY)
+        MAILCHIMP_API_KEY = 'Mailchimp API Key Goes Here'
+        mailchimp_auth = ("", MAILCHIMP_API_KEY)
 
-    response = requests.post(mailchimp_url, json=mailchimp_data, auth=mailchimp_auth)
+        response = requests.post(mailchimp_url, json=mailchimp_data, auth=mailchimp_auth)
 
-    # Return the token to the client
-    data = { 'token': token.key }
-    response['X-CSRFToken'] = csrf_token
-    response["Access-Control-Allow-Origin"] = "http://localhost:3000"
-    return Response(data)
+        # Return the token to the client
+        # token = Token.objects.create(user=new_user)
+        token = Token.objects.filter(user=new_user).first()
+        if token:
+            # If a token exists, delete it to create a new one
+            token.delete()
+        # Create a new token for the user
+        token = Token.objects.create(user=new_user)
+        response = JsonResponse({'token': token.key})
+        response['X-CSRFToken'] = get_token(request)
+        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        return response
+    else:
+        return HttpResponseNotAllowed(['POST'])
